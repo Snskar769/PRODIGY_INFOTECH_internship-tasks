05939b3f55344f94e83ed0ed536b16f85f2fdf96
Snskar769 <154292061+Snskar769@users.noreply.github.com>
2026-01-16 17:39:20 +0600

    Add files via upload
    # Task 03: Password Complexity Checker
    The Password Complexity Checker is a security tool designed to educate users on what makes a "strong" password. By analyzing specific characteristics of a string, the tool provides immediate feedback on whether a password is weak, medium, or strong based on industry-standard security patterns.

---
+++ PRODIGY_CS_03.py
@@ -0,0 +1,47 @@
+
+def pass_complexity_chck(passwd):
+    has_upper= False
+    has_lower= False
+    has_num= False
+    has_special= False
+    special_ch= "@#$_ /"
+
+    for char in passwd:
+        if char.isupper():
+            has_upper = True
+        elif char.islower():
+            has_lower = True
+        elif char.isdigit():
+            has_num = True
+        elif char in special_ch:
+            has_special = True
+    leng= 7<len(passwd)<21
+    
+    dict = [
+        (has_upper, "Uppercase letters (A-Z)"),
+        (has_lower, "Lowercase letters (a-z)"),
+        (has_num, "Numbers (0-9)"),
+        (has_special, f"Special characters ({special_ch})")]
+    
+    score = sum(1 for met, label in dict if met)
+    print("\n--- PASSWORD STRENGTH ---")
+    
+    if not leng:
+        print("Strength: INVALID ")
+        print("Please Enter password between 8-20 characters !!")
+    else:
+        if score == 4:
+            print("Strength: STRONG ")
+        elif score == 3:
+            print("Strength: MEDIUM ")
+        else:
+            print("Strength: WEAK ")
+        if score < 4:
+            print("\nTo improve your score, include:")
+            for met, label in dict:
+                if not met:
+                    print(f" [ ] {label}")
+
+
+password =input("Enter your password between 8-20 characters\nIt should contain atleast one uppercase character\nIt should contain atleast one lowercase character \nIt should contain atleast one number\n \n Pass :")
+pass_complexity_chck(password)
